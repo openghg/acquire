5f01fc881bb871fb6e23f32cc9981a3f6dd6abbb
Gareth Jones <oxidising@gmail.com>
2021-07-27 15:45:06 +0100

    WIP: More replacements

--- test/Service/test_helpers.py
+++ test/Service/test_helpers.py
@@ -6,18 +6,27 @@ from Acquire.Service import get_service_url
 @mock.patch.dict(os.environ, {"ACQUIRE_HOST": "acquire.example.org"})
 def test_get_service_url():
     url = get_service_url()
-    assert url == "acquire.example.org"
+    assert url == "https://acquire.example.org"
 
-    identity_url = get_service_url(service="identity")
+    identity_url = get_service_url(service="identity", https=False)
 
     assert identity_url == "acquire.example.org/t/identity"
+
+    identity_url = get_service_url(service="identity", https=True)
+
+    assert identity_url == "https://acquire.example.org/t/identity"
 
 
 @mock.patch.dict(os.environ, {"ACQUIRE_HOST": "https://acquire.example.org"})
 def test_get_service_url_with_https():
     url = get_service_url()
-    assert url == "acquire.example.org"
+    assert url == "https://acquire.example.org"
 
-    identity_url = get_service_url(service="identity")
+    identity_url = get_service_url(service="identity", https=False)
 
     assert identity_url == "acquire.example.org/t/identity"
+
+    identity_url = get_service_url(service="identity", https=True)
+
+    assert identity_url == "https://acquire.example.org/t/identity"
+
